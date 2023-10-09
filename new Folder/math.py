6e069a83928576a12811e154a8d5b757d56b0130
Hanmant1999 <hpetekar@gmail.com>
2023-10-09 23:28:26 +0530

    math class added

---
+++ new Folder/math.py
@@ -0,0 +1,22 @@
+
+class Math :
+     
+    def __init__(self,a,b):
+            self.a =a 
+            self.b = b
+             
+    def add(self):
+        return self.a+self.b
+    
+    def subtract(self):
+        return self.a-self.b
+    
+    def multiple(self):
+        return self.a*self.b
+    
+    def division(self):
+        return self.a/self.b
+    
+    def remainder(self):
+        return self.a%self.b
+    
